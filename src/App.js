2938c03c1dd1a05c37dc360b60410a1bb84e1152
Trusevich <ITrusevich@alluma.org>
2020-06-12 14:43:55 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const load = () => {
+    console.log('LOAD')
+
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(json => {
+    //       setUsers(json);
+    //     })
+    // }
+    axios({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/users',
+    })
+      .then((response) => {
+        setUsers(response.data);
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={load}> Load todos</button>
+
+      <ul>
+
+        {
+          users.map(el => <li key={el.id}>{el.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
